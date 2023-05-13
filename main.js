b04b190d4d04a5845e7ad76b96a2b6274863c380
Rory Martin <rorymartin3d@gmail.com>
2023-05-13 13:52:01 +0100

    Created UI

--- main.js
+++ main.js
@@ -1,66 +1,162 @@
-// Rock Paper Scissors
-
+//// Rock Paper Scissors
 // Counters to keep scores
 let playerScore = 0;
 let computerScore = 0;
+let roundWinner = '';
+// get the buttons
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+// get the div for outputs
+const roundResult = document.getElementById('round-result'); // Displays winner
+const finalResult = document.getElementById('score-info'); // shows who chose what
+const playerSign = document.getElementById('player-sign');
+const computerSign = document.getElementById('computer-sign');
+const playerScoreOutput = document.getElementById('player-score');
+const computerScoreOutput = document.getElementById('computer-score');
+const scoreMessage = document.getElementById('score-message');
+const endGameModal = document.getElementById('modal-container');
+const endGameMessage = document.getElementById('end-game-message');
+const overlay = document.getElementById('overlay');
+const restartButton = document.getElementById('restart-button');
+// Add Event Listeners
+rockButton.addEventListener('click', () => handleClick('ROCK'));
+paperButton.addEventListener('click', () => handleClick('PAPER'));
+scissorsButton.addEventListener('click', () => handleClick('SCISSORS'));
+restartButton.addEventListener('click', restartGame);
+overlay.addEventListener('click', closeEndGameModal);
 
 // Computer picks between rock, paper or scissors by selecting random array item, answer is returned
 function getComputerChoice() {
-  let computerOptions = ["Rock", "Paper", "Scissors"];
-  let computerRandom = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-  return computerRandom;
+    let computerOptions = ['Rock', 'Paper', 'Scissors'];
+    let computerRandom = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+    return computerRandom;
 }
 
-// Play a round. First Player picks, then computer.
-//then compare options, return win or lose text and increment score
-function playRound() {
-  const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-  const computerSelection = getComputerChoice().toLowerCase();
-
-  console.log("computer choice: " + computerSelection);
-
-  if (playerSelection === computerSelection) {
-    return "Draw! Both players chose " + playerSelection;
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    playerScore++;
-    return "You Win!  " + playerSelection + " Beats " + computerSelection;
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    computerScore++;
-    return "You Lose! Paper beats Rock";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerScore++;
-    return "You Win! Scissors beats Paper";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    computerScore++;
-    return "You Lose! Rock beats Scissors";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerScore++;
-    return "You Win! Paper beats Rock";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    computerScore++;
-    return "You Lose! Scissors Beat Paper";
-  } else {
-    return "You must make a valid choice";
-  }
+function isGameOver() {
+    return playerScore === 5 || computerScore === 5;
 }
 
-// Repeat playRound 5 times. I am using clog to keep track of what is happening here.
-for (let round = 1; round <= 5; round++) {
-  console.log(playRound());
-  console.log("round: " + round);
-  console.log("playerScore " + playerScore);
-  console.log("computerScore " + computerScore);
+function handleClick(playerSelection) {
+    if (isGameOver()) {
+        openEndGameModal();
+        return;
+    }
+
+    const computerSelection = getComputerChoice().toUpperCase();
+    playRound(playerSelection, computerSelection);
+    updateChoices(playerSelection, computerSelection);
+    updateScore();
+
+    if (isGameOver()) {
+        openEndGameModal();
+        setFinalMessage();
+    }
 }
 
-// compare player score and computer score and return who is the winner.
-function result(playerScore, computerScore) {
-  if (playerScore > computerScore) {
-    console.log("Player wins!");
-  } else if (playerScore < computerScore) {
-    console.log("Computer wins!");
-  } else {
-    console.log("Draw");
-  }
+function updateChoices(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case 'ROCK':
+            playerSign.textContent = '✊';
+            break;
+        case 'PAPER':
+            playerSign.textContent = '✋';
+            break;
+        case 'SCISSORS':
+            playerSign.textContent = '✌';
+            break;
+    }
+
+    switch (computerSelection) {
+        case 'ROCK':
+            computerSign.textContent = '✊';
+            break;
+        case 'PAPER':
+            computerSign.textContent = '✋';
+            break;
+        case 'SCISSORS':
+            computerSign.textContent = '✌';
+            break;
+    }
 }
 
-console.log(result(playerScore, computerScore));
+function playRound(playerSelection, computerSelection) {
+    playerSign.innerText = playerSelection;
+    computerSign.innerText = computerSelection;
+
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie';
+    }
+    if (
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    ) {
+        playerScore++;
+        roundWinner = 'player';
+    }
+    if (
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+    ) {
+        computerScore++;
+        roundWinner = 'computer';
+    }
+    updateScoreMessage(roundWinner, playerSelection, computerSelection);
+}
+
+function updateScore() {
+    if (roundWinner === 'tie') {
+        roundResult.textContent = "It's a draw!";
+    } else if (roundWinner === 'player') {
+        roundResult.textContent = 'You Won!';
+    } else if (roundWinner === 'computer') {
+        roundResult.textContent = 'You lost!';
+    }
+    playerScoreOutput.textContent = `Player: ${playerScore}`;
+    computerScoreOutput.textContent = `Computer: ${computerScore}`;
+}
+
+function updateScoreMessage(winner, playerSelection, computerSelection) {
+    if (winner === 'player') {
+        scoreMessage.textContent = `${capitalizeFirstLetter(playerSelection)} beat ${computerSelection.toLowerCase()}`;
+        return;
+    }
+    if (winner === 'computer') {
+        scoreMessage.textContent = `${capitalizeFirstLetter(playerSelection)} is beaten by ${computerSelection.toLowerCase()}`;
+        return;
+    }
+    scoreMessage.textContent = `${capitalizeFirstLetter(playerSelection)} ties with ${computerSelection.toLowerCase()}`;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function openEndGameModal() {
+    endGameModal.style.display = 'flex';
+    overlay.style.display = 'block';
+}
+
+function closeEndGameModal() {
+    endGameModal.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+function setFinalMessage() {
+    return playerScore > computerScore ? (endGameMessage.textContent = 'You won!') : (endGameMessage.textContent = 'You lost...');
+}
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    roundResult.textContent = 'Make Your Choice';
+    scoreMessage.textContent = 'First to score 5 points wins the game';
+    playerScoreOutput.textContent = 'Player: 0';
+    computerScoreOutput.textContent = 'Computer: 0';
+    playerSign.textContent = '❔';
+    computerSign.textContent = '❔';
+    endGameModal.style.display = 'none';
+    overlay.style.display = 'none';
+}
